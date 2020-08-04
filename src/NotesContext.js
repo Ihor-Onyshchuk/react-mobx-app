a2dbaf02761ce91f5344dcf0c825e2723f51192e
ihor <onyshchukihor11@gmail.com>
2020-08-04 15:35:27 +0300

    add main logic

--- src/NotesContext.js
+++ src/NotesContext.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { createNotesStore } from './notesStore';
+import { useLocalStore } from 'mobx-react';
 
 const NotesContext = React.createContext(null);
 
 export const NotesProvider = ({ children }) => {
-  const notesStore = createNotesStore();
+  const notesStore = useLocalStore(createNotesStore);
 
   return (
     <NotesContext.Provider value={notesStore}>
